76e1194946627a20bd1fa9a237e41955f7f0317f
divenier <charles5655@163.com>
2020-12-27 15:37:09 +0800

    修改了ignore文件 新建receiver

---
+++ src/FileReceiverDemo01.java
@@ -0,0 +1,9 @@
+/**
+ * @author divenier
+ * @date 2020/12/27 15:29
+ */
+public class FileReceiverDemo01 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/FileSenderDemo01.java
+++ src/FileSenderDemo01.java
@@ -1,4 +1,6 @@
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 
 /**
@@ -8,6 +10,18 @@ import java.net.Socket;
 public class FileSenderDemo01 {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost",6666);
-        System.out.println("我忘了怎么写的了");
+        String filePath = "./resource/kkb.mkv";
+        try(FileInputStream fis = new FileInputStream(filePath);
+            OutputStream os = socket.getOutputStream()){
+
+            int len = 0;
+            byte[] data = new byte[1024];
+            while((len = fis.read(data)) > -1){
+                os.write(data);
+            }
+            System.out.println("file write done");
+        }
+        socket.close();
+        System.out.println("the file sender disconnected");
     }
 }
