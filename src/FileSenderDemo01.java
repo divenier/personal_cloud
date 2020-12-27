e03f00f23d8bf365142eda910fc3226d07686f64
divenier <charles5655@163.com>
2020-12-27 10:32:35 +0800

    I dont want to do it anymore

---
+++ src/FileSenderDemo01.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ * @author divenier
+ * @date 2020/12/27 10:23
+ */
+public class FileSenderDemo01 {
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost",6666);
+        System.out.println("我忘了怎么写的了");
+    }
+}
