1d818863b80c2140016a7c03b4f00ca70e1d2191
divenier <charles5655@163.com>
2021-02-16 15:09:42 +0800

    编写常量工具类

---
+++ src/demo_with_database/utils/Constants.java
@@ -0,0 +1,11 @@
+package demo_with_database.utils;
+
+/**
+ * @author divenier
+ * @date 2021/2/15 22:29
+ * 用来放置一些常量
+ */
+public class Constants {
+    //服务器端口号
+    public final static int SERVER_PORT = 6666;
+}
